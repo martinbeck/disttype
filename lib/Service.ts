14e7a333445b52961e6a16d6b0e3d5c6c57a4d30
Martin Beck <maddin.beck@gmail.com>
2018-11-05 15:14:17 +0100

    wip

--- lib/Service.ts
+++ lib/Service.ts
@@ -1,3 +1,3 @@
-export class Service {
-
+export default class Service {
+  
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,30 @@
-export function distributedApp(fn: () => void): void {
+import { promisify } from 'util';
+import Service from './Service';
+
+type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any;
+type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T];
+
+type RemoteService<T> = {
+  [K in keyof T]: T[K] extends Function ? T[K] : never;
+}
+
+function createService<T>(creator: () => T): RemoteService<T> {
+  const service = creator();
+  const proxy = new Proxy({}, { get: (_, property: string) => (...argumentList: any[]) => {
+    const method: any = (service as any)[property];
+      if (typeof method === 'function') {
+        return method.apply(service, argumentList);
+      }
+  } })
+  return proxy as RemoteService<T>;
+}
+
+function distributedApp(fn: () => void): void {
   fn();
+}
+
+export {
+  createService,
+  distributedApp,
+  Service
 }
